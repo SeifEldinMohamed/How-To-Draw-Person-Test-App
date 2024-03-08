7f5856b0c80b0af1adce68a6cc89a47a0c4792b7
Seif <seifeldinh@banquemisr.com>
2024-03-08 21:05:47 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "How To Draw Person Test App"
+include(":app")
+ 
