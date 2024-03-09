7dea711eca17281c5b715718a6116095a475cba1
Seif <seifeldinh@banquemisr.com>
2024-03-09 21:08:10 +0200

    add Typography

---
+++ app/src/main/java/com/seif/howtodrawpersontestapp/ui/theme/Type.kt
@@ -0,0 +1,55 @@
+package com.seif.howtodrawpersontestapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        textDirection = TextDirection.Rtl
+    ),
+)
